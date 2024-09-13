6a6f365ac02b2f2badf65f35fbff4d2de4749059
Prabzzz <prabhakaranjayakanthan@gmail.com>
2024-09-13 15:16:53 +0530

    updated logo

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Trident - Dental Clinic React Website
+
+**Trident** is a Modern Responsive Website for Dental Clinic. It is built
+with ReactJS, Bootstrap, and Sass. Dental clinic can use
+this template to bring their business online or increase business visibility.
+
+## Live View
+
+## Tech Stack
+React, Bootstrap, SASS, React Router, React Slick, React Icons and AOS
+
+**Hosting:** Vercel
+
+## Website Features
+
+- Fully Responsive
+- Built with React
+- Bootstrap 5
+- Speed Optimized
+- Smooth Animation
+
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Navbar.scss';
+import logo from './../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const navbarItems = [
+        {
+            name: 'Home',
+            path: '/',
+        },
+        {
+            name: 'About Us',
+            path: '/about',
+        },
+        {
+            name: 'Single Service',
+            path: '/singleservice',
+        },
+        {
+            name: 'Blogs',
+            path: '/blogs',
+        },
+        {
+            name: 'Contact Us',
+            path: '/contact',
+        }
+    ];
+
+    const phoneNumber = '+919944465304'; // Replace with your actual phone number
+    const bookingMessage = 'Hello, I would like to book an appointment.'; // Replace with your booking message
+    const encodedMessage = encodeURIComponent(bookingMessage);
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+    return (
+        <div className='main-nav'>
+            <div className="container">
+                <nav className="navbar navbar-expand-lg">
+                    <div className="container-fluid">
+                        {/* Logo */}
+                        <Link className="navbar-brand" to="/">
+                            <img src={logo} alt="logo" />
+                        </Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            {/* Navbar Link */}
+                            <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+                                {
+                                    navbarItems.map(navSingle =>
+                                        <li className="nav-item">
+                                            <Link className="nav-link" to={navSingle.path}>{navSingle.name}</Link>
+                                        </li>
+                                    )
+                                }
+                            </ul>
+
+                            {/* Navbar Button */}
+                            <div className="theme-btn">
+                                <a href={whatsappUrl} target="_blank" rel="noopener noreferrer"> Book Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
