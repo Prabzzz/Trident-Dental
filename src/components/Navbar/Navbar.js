bca474b2cb14dea22723cf45005b21513bb08d6b
Prabzzz <prabhakaranjayakanthan@gmail.com>
2025-04-03 09:47:16 +0530

    services corrected

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
             path: '/about',
         },
         {
-            name: 'Single',
+            name: 'Services',
             path: '/service',
         },
         {
